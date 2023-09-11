f061374bef64b44024ca178c79e32e53ce20eee6
dkhasnudin <dzikri.khasnudin2@gmail.com>
2023-09-11 12:50:32 +0700

    modifikasi halaman detail frontpage

--- resources/views/frontpage/detail.blade.php
+++ resources/views/frontpage/detail.blade.php
@@ -26,21 +26,34 @@
             <div class="grid gap-12 sm:grid-cols-2 lg:grid-cols-4 justify-center">
                 @foreach ($relatedArticles as $related)
                 <article class="max-w-xs">
-                    <a href="#">
+                    @if ($title == 'program')
+                    <a href="{{ route('detail', ['category' => $title, 'slug' => $related->slug]) }}">
                         <img src="{{ $related->thumbnail }}" class="w-full aspect-video object-cover mb-5 rounded-lg"
-                            alt="Image 1">
+                            alt="{{ $related->title }}">
                     </a>
                     <h2 class="mb-2 text-lg font-bold leading-tight text-gray-900 dark:text-white">
-                        <a href="#" class="line-clamp-2">{{ $related->title }}</a>
+                        <a href="{{ route('detail', ['category' => $title, 'slug' => $related->slug]) }}"
+                            class="line-clamp-2">{{ $related->title }}</a>
                     </h2>
                     <p class="mb-4 font-light text-gray-500 dark:text-gray-400 line-clamp-3">{{ $related->description }}
                     </p>
-                    @if ($title == 'program')
+
                     <a href="{{ route('detail', ['category' => $title, 'slug' => $related->slug]) }}"
                         class="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline">
                         Baca selengkapnya
                     </a>
                     @else
+                    <a href="{{ route('detail', ['category' => $related->category, 'slug' => $related->slug]) }}">
+                        <img src="{{ $related->thumbnail }}" class="w-full aspect-video object-cover mb-5 rounded-lg"
+                            alt="{{ $related->title }}">
+                    </a>
+                    <h2 class="mb-2 text-lg font-bold leading-tight text-gray-900 dark:text-white">
+                        <a href="{{ route('detail', ['category' => $related->category, 'slug' => $related->slug]) }}"
+                            class="line-clamp-2">{{ $related->title }}</a>
+                    </h2>
+                    <p class="mb-4 font-light text-gray-500 dark:text-gray-400 line-clamp-3">{{ $related->description }}
+                    </p>
+
                     <a href="{{ route('detail', ['category' => $related->category, 'slug' => $related->slug]) }}"
                         class="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline">
                         Baca selengkapnya
